b192ec419a9f0eccc863f60169da5fc5acee33f9
GarimaGrewal <grewalgarima@gmail.com>
2021-04-17 14:54:26 +0530

    Update MagicNumber.java

--- MagicNumber.java
+++ MagicNumber.java
@@ -1,6 +1,11 @@
 // { Driver Code Starts
 //Initial Template for Java
 
+
+// A number is called faithful if you can write it as the sum of distinct powers of 7. 
+// e.g.,  2457 = 7 + 72 + 74 . If we order all the faithful numbers, we get the sequence 1 = 70, 7 = 71, 8 = 70 + 71, 49 = 72, 50 = 70 + 72 . . . and so on.
+// Given some value of N, you have to find the N'th faithful number.
+
 import java.io.*;
 import java.util.*;
 
